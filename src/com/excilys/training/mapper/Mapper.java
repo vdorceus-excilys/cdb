a1878cffe9b168ed9cb8ef79c3a138334a60d577
vdorceus-excilys <slavole@excilys.com>
2019-04-19 16:49:51 +0200

    fresh repo

--- src/com/excilys/training/mapper/Mapper.java
+++
@@ -1,8 +0,0 @@
-package com.excilys.training.mapper;
-
-public interface Mapper<M,S> {
-	
-	public M forward(S pojo);
-	public S reverse(M pojo);	
-	
-}
--- src/com/excilys/training/mapper/dto/DataTransferObject.java
+++
@@ -1,5 +0,0 @@
-package com.excilys.training.mapper.dto;
-
-public interface DataTransferObject <T> extends Comparable<DataTransferObject <T>>{
-
-}
